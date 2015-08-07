0f98e6e6a7255ae031649dbace20968a41e8a5ad
joanaz <rz258@cornell.edu>
2015-08-07 12:24:57 -0400

    c

---
+++ server/app/routes/pages/index.js
@@ -0,0 +1,11 @@
+'use strict';
+var router = require('express').Router();
+var mongoose = require('mongoose');
+var Page = mongoose.model('Page');
+
+module.exports = router;
+
+
+router.get('/', function(req, res, next) {
+
+});
